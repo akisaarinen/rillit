af8e3707292f9e3541c5c53cfa1c9adcc617e914
Aki Saarinen <aki@akisaarinen.fi>
2012-12-06 18:11:10 +0200

    testing: Update to more realistic example
    Signed-off-by: Aki Saarinen <aki@akisaarinen.fi>

--- testing/src/main/scala/rillit/TestLens.scala
+++ testing/src/main/scala/rillit/TestLens.scala
@@ -1,19 +1,34 @@
 package rillit
 
-case class Inner(value: Int)
-case class Outer(inner: Inner)
+
+case class Electronic(email: String, web: String)
+case class Contact(electronic: Electronic)
+case class Name(first: String, last: String)
+case class Person(name: Name, age: Int, contact: Contact)
 
 object Main {
-  val outer = Outer(Inner(3))
+  val aki = Person(
+    name = Name("Aki", "Saarinen"),
+    age  = 27,
+    contact = Contact(
+      electronic = Electronic(
+        email = "aki@akisaarinen.fi",
+        web   = "http://akisaarinen.fi"
+      )
+    )
+  )
+
+  val email      = new Lenser[Electronic].email
+  val electronic = new Lenser[Contact].electronic
+  val contact    = new Lenser[Person].contact
 
   def main(args: Array[String]) {
-    println("--macro--")
-    val value = new Lenser[Inner].value
-    val inner = new Lenser[Outer].inner
-    val composedLens = inner andThen value
-    println("Original:       %s".format(outer))
-    println("Original value: %s".format(composedLens.get(outer)))
-    println("Modified:       %s".format(composedLens.set(outer, 4)))
+    val lens = contact andThen electronic andThen email
+
+    val aki2 = lens.set(aki, "aki2@akisaarinen.fi")
+
+    println("Original: %s".format(lens.get(aki)))
+    println("Updated:  %s".format(lens.get(aki2)))
   }
 }
 
